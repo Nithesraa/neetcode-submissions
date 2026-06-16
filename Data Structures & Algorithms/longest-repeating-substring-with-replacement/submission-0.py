abfdcb24a50e01a7b4b0141dbdd2b9461f06d302
Nithesraa <nithesraa006@gmail.com>
2026-06-16 20:11:12 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,16 @@
+from collections import Counter
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = Counter()
+        l = 0
+        max_frq = 0
+        res = 0
+        for r in range(len(s)):
+            count[s[r]]+= 1
+            max_frq = max(max_frq,count[s[r]])
+            while ((r-l+1) - max_frq ) > k:
+                count[s[l]] -=1
+                l+=1
+            res = max(res,r-l+1)
+        return res
+        
